addcc86be1692111e762ea6c3a9dad1579c4dc78
Manish Dewangan <manishdewangan1302@gmail.com>
2026-06-22 16:41:11 +0530

    Created all the pages and defined Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { Toaster } from "react-hot-toast";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Employees from "./pages/Employees";
+import Attendance from "./pages/Attendance";
+import Leave from "./pages/Leave";
+import Payslips from "./pages/Payslips";
+import PrintPayslip from "./pages/PrintPayslip";
+import Settings from "./pages/Settings";
+import LoginLanding from "./pages/LoginLanding";
+import { Layout } from "lucide-react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={<LoginLanding />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/payslips" element={<Payslips />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+        <Route path="/print/payslip/:id" element={<PrintPayslip />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/pages/LoginLanding.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginLanding = () => {
+  return <div></div>;
+};
+
+export default LoginLanding;
