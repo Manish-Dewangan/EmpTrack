f73a6eccd68e97a16328fee5b826dc7b03def0b5
Manish Dewangan <manishdewangan1302@gmail.com>
2026-06-27 16:13:10 +0530

    Attendance Page UI completed

--- client/src/pages/Employees.jsx
+++ client/src/pages/Employees.jsx
@@ -22,11 +22,11 @@ const Employees = () => {
     setTimeout(() => {
       setLoading(false);
     }, 1000);
-  }, []);
+  }, [selectedDept]);
 
   useEffect(() => {
     fetchEmployees();
-  }, []);
+  }, [fetchEmployees]);
 
   const filtered = employee.filter((emp) =>
     `${emp.firstName} ${emp.lastName} ${emp.position}`
